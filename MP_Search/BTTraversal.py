086a25d20c7bc311375f0ec5525841f4d4af3f3c
pengmeng <mengp3157@gmail.com>
2015-01-14 23:56:44 -0600

    BT inorder and postorder traversal

--- MP_Search/BTTraversal.py
+++ MP_Search/BTTraversal.py
@@ -25,9 +25,45 @@ def preorder_my(root):
     result = []
     while stack:
         node = stack.pop()
-        result.append(node.val)
-        if node.right:
+        if node:
+            result.append(node.val)
             stack.append(node.right)
-        if node.left:
             stack.append(node.left)
+    return result
+
+
+def inorder(root):
+    if not root:
+        return []
+    stack = [root]
+    result = []
+    while stack:
+        node = stack.pop()
+        while node:
+            stack.append(node)
+            node = node.left
+        if stack:
+            node = stack.pop()
+            result.append(node.val)
+            stack.append(node.right)
+    return result
+
+
+def postorder(root):
+    if not root:
+        return []
+    stack = [root]
+    pre = root
+    result = []
+    while stack:
+        node = stack.pop()
+        if (not node.left and not node.right) or pre is node.left or pre is node.right:
+            result.append(node.val)
+            pre = node
+        else:
+            stack.append(node)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
     return result
--- MP_Search/test_bttraversal.py
+++ MP_Search/test_bttraversal.py
@@ -23,3 +23,20 @@ class TestPreorder(TestCase):
         self.assertEqual([1, 2, 3, 2, 3], BTTraversal.preorder_my(root))
         root = TreeNode.makeTree([3, 1, 2])
         self.assertEqual([3, 1, 2], BTTraversal.preorder_my(root))
+
+    def test_inorder(self):
+        root = TreeNode.makeTree([1, '#', 2, 3])
+        self.assertEqual([1, 3, 2], BTTraversal.inorder(root))
+        root = TreeNode.makeTree([1, 2, 3, 4, 5, '#', 6])
+        self.assertEqual([4, 2, 5, 1, 3, 6], BTTraversal.inorder(root))
+        root = TreeNode.makeTree([3, 1, 4, '#', 2, '#', 5, '#', '#', '#', 6])
+        self.assertEqual([1, 2, 3, 4, 5, 6], BTTraversal.inorder(root))
+
+    def test_postorder(self):
+        self.assertEqual([], BTTraversal.postorder([]))
+        root = TreeNode.makeTree([1, '#', 2, 3])
+        self.assertEqual([3, 2, 1], BTTraversal.postorder(root))
+        root = TreeNode.makeTree([1, 2, 2, '#', 3, '#', 3])
+        self.assertEqual([3, 2, 3, 2, 1], BTTraversal.postorder(root))
+        root = TreeNode.makeTree([3, 1, 2])
+        self.assertEqual([1, 2, 3], BTTraversal.postorder(root))
